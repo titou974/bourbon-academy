e3950c69931fd0d2cc0e85c25e867d7a5dd90bd0
titou974 <titouanhirsch@gmail.com>
2026-03-14 00:21:01 +0100

    map reunion corrected

--- src/components/bourbon/HeroAnimation.tsx
+++ src/components/bourbon/HeroAnimation.tsx
@@ -12,7 +12,6 @@ import { useEffect, useRef } from "react";
 
 export function HeroAnimation() {
   const animationRefFrance = useRef<LottieRefCurrentProps>(null);
-  const animationRefReunion = useRef<LottieRefCurrentProps>(null);
   const DELAY_MAP_ONE = 1.15;
   const DELAY_MAP_TWO = 1.25;
   const DELAY_LOTTIE = 1.4;
@@ -26,18 +25,9 @@ export function HeroAnimation() {
       },
       (DELAY_LOTTIE + 0.15) * 1000,
     );
-    const visibilityTimerReunion = setTimeout(
-      () => {
-        if (animationRefReunion.current) {
-          animationRefReunion.current.play();
-        }
-      },
-      (DELAY_LOTTIE + 0.3) * 1000,
-    );
 
     return () => {
       clearTimeout(visibilityTimerFrance);
-      clearTimeout(visibilityTimerReunion);
     };
   }, []);
   return (
@@ -52,19 +42,28 @@ export function HeroAnimation() {
         }}
         className="relative flex flex-col-reverse md:flex-col items-center gap-2"
       >
-        <Image
-          width={200}
-          height={200}
-          src="/france-map.png"
-          alt="France"
-          className="md:w-[200px] w-[100px] "
-        />
+        <div className="flex items-center justify-center">
+          <Image
+            width={200}
+            height={200}
+            src="/france-map.png"
+            alt="France"
+            className="md:w-[150px] w-[100px]"
+          />
+          <Image
+            width={150}
+            height={150}
+            src="/reunion-island-map.png"
+            alt="Réunion"
+            className="hidden md:block"
+          />
+        </div>
         <Image
           width={200}
           height={200}
           src="/reunion-island-map.png"
           alt="Réunion"
-          className="md:w-[50px] w-[50px] absolute md:top-[-1em] top-[3em] right-[-1em]"
+          className="md:hidden w-[50px] absolute md:top-[-1em] top-[3em] right-[-1em]"
         />
         <p className="text-muted-foreground text-center text-sm z-20">
           France et DROM-COM
@@ -75,14 +74,7 @@ export function HeroAnimation() {
         animationData={maplineAnimation}
         loop={false}
         autoplay={false}
-        className="md:w-[20em] w-[8em] mx-auto rotate-110 md:rotate-28 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-      />
-      <Lottie
-        lottieRef={animationRefReunion}
-        animationData={maplineAnimation}
-        loop={false}
-        autoplay={false}
-        className="md:w-[16em] w-[8em] mx-auto rotate-110 md:rotate-35 absolute top-11 right-[-1.1em] transform -translate-x-1/2 -translate-y-1/2 hidden md:block"
+        className="md:w-[16em] w-[8em] mx-auto rotate-110 md:rotate-20 absolute top-1/2 left-1/2 md:left-[60%] transform -translate-x-1/2 -translate-y-1/2"
       />
       <motion.div
         initial={{ opacity: 0, y: 20 }}
